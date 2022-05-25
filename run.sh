5ea3463594289fe861b8ba75deb36d58a3e87d2f
ugonfor <ugonfor@gmail.com>
2022-05-25 21:35:59 +0900

    Update run script

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ pip install -r requirements.txt
 wget https://grammarly-nlp-data-public.s3.amazonaws.com/gector/roberta_1_gectorv2.th
 
 # test data
-echo "this is incrorect sentences." > input
+echo "this is incrorect sentences.
+This is incrrect sentences." > input
 
 python gec.py --input_file ./input --output_file ./output --batch_size 32 --model_path ./roberta_1_gectorv2.th --model_name t5-base
