4607cad8953e630e047113d8589464aaac305d84
ugonfor <ugonfor@gmail.com>
2022-06-03 15:15:29 +0900

    Update data/

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,5 +1,3 @@
-from numpy import source
-
 
 data = './fce/m2/fce.test.gold.bea19.m2'
 
--- data/scoring.sh
+++
@@ -1,5 +0,0 @@
-# annotation
-errant_parallel -orig ./example/example.input -cor ./example/example.output -out ./example/example.hyp
-
-# scoring
-errant_compare -hyp ./example/example.hyp -ref ./example/example.ref
