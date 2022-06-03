a6af209e7e300b52d3d75ddcd495f0fa4c988d7f
ugonfor <ugonfor@gmail.com>
2022-06-03 16:05:12 +0900

    Update eval (jfleg)

--- eval/bea19/download.sh
+++ eval/bea19/download.sh
@@ -4,3 +4,6 @@ tar xvf fce_v2.1.bea19.tar.gz
 python preprocess.py
 
 python m2-correction.py ./fce/m2/fce.test.gold.bea19.m2 > ./gold.bea19
+
+rm ./fce_v2.1.bea19.tar.gz
+rm -rf ./fce
---
+++ eval/jfleg/download.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/keisks/jfleg
+
+mv ./jfleg/test .
+rm -rf ./jfleg
