ab9dd085b3c234393251c626ed2eff50e5962d8c
ugonfor <ugonfor@gmail.com>
2022-06-03 15:34:50 +0900

    Update Evaluation Method

--- eval/bea19/download.sh
+++ eval/bea19/download.sh
@@ -2,3 +2,5 @@ wget https://www.cl.cam.ac.uk/research/nl/bea2019st/data/fce_v2.1.bea19.tar.gz
 tar xvf fce_v2.1.bea19.tar.gz
 
 python preprocess.py
+
+python m2-correction.py ./fce/m2/fce.test.gold.bea19.m2 > ./gold.bea19
--- eval/bea19/preprocess.py
+++ eval/bea19/preprocess.py
@@ -2,7 +2,7 @@
 data = './fce/m2/fce.test.gold.bea19.m2'
 
 src = open(data, "rb")
-dst = open("./input.bea2019", "wb")
+dst = open("./input.bea19", "wb")
 
 for line in src:
     if line[0] == ord('S'):
